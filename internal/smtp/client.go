6785659d67295758b59fc9bd2bd9224986117053
mauricio-gallera <galmeidamauricio12@gmail.com>
2026-09-19 19:49:34 -0300

    smtp connection

---
+++ internal/smtp/client.go
@@ -0,0 +1,97 @@
+package protocol
+
+import ("net/smtp"
+		"fmt"
+		"crypto/tls"
+		"strings"
+		"log"
+	
+		"github.com/mau0414/deliverability-relay/internal/domain")
+
+type Connection struct {
+
+	addr string
+	client *smtp.Client
+
+}
+
+func NewConnection(addr string) (*Connection, error) {
+
+	smtpClient, err := smtp.Dial(addr) 
+	
+	if err != nil {
+		return nil, fmt.Errorf("SMTP Connection to address %s failed: %w", addr, err)
+	}
+	
+	return &Connection{
+		addr: addr,
+		client: smtpClient,
+	}, nil
+
+}
+
+func protocolFormat(email domain.Email) string {
+	toHeader := strings.Join(email.To, ", ")
+
+	return fmt.Sprintf(
+		"From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s",
+		email.From,
+		toHeader,
+		email.Subject,
+		email.HTML,
+	)
+}
+
+func (c *Connection) Deliver(mtaDomain string, email domain.Email) error {
+
+	// to guarantee connection close afterwards
+	defer c.client.Close()
+
+	// Hello to server
+	if err := c.client.Hello(mtaDomain); err != nil {
+		return fmt.Errorf("HELO command failed: %w", err)
+	}
+	
+	// StartTLS to make content encrypted 
+	tlsConfig := &tls.Config{ServerName: mtaDomain}
+	if err := c.client.StartTLS(tlsConfig); err != nil {
+		return fmt.Errorf("STARTTLS command failed: %w", err) // things could be more graceful here but for a study case project tls servers only will be considered
+	} 
+
+	// inform from to server
+	if err := c.client.Mail(email.From); err != nil {
+		return fmt.Errorf("FROM command failed: %w", err)
+	} 
+
+	// Inform each recipient to server
+	for _, to := range email.To {
+		if err := c.client.Rcpt(to); err != nil {
+			return fmt.Errorf("TO command failed: %w", err)
+		}
+	}
+
+	// DATA command to get writer
+	formattedEmail := protocolFormat(email)
+	writer, err := c.client.Data()
+
+	if err != nil {
+		return fmt.Errorf("DATA command failed: %w", err)
+	}
+
+	// writing email at correct protocol format
+	if _, err := writer.Write([]byte(formattedEmail)); err != nil {
+			return fmt.Errorf("failed to write email body: %w", err)
+	}
+
+	if err := writer.Close(); err != nil {
+		return fmt.Errorf("failed to close data writer: %w", err)
+	}
+
+	// quit connection to server
+	if err := c.client.Quit(); err != nil {
+		log.Printf("warning: QUIT failed (email likely already delivered): %v", err)
+	}
+
+	return nil
+
+}
